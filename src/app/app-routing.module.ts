243c4d2396c077260fc73351f101931fe474e6f8
dertufa <tuliosouza2011@gmail.com>
2023-06-26 19:50:15 -0300

    adição do componente instituição

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { InstituicaoComponent } from './components/instituicao/instituicao.component';
+
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {path:'',component: HomeComponent}
+  {path:'',component: HomeComponent},
+  {path:'instituicao',component: InstituicaoComponent}
 
 ];
 
