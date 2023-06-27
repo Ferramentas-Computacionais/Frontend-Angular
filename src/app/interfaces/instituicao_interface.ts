a6a6c362078ea36f098e0b737cd4733fdee74270
kalebeh2o <kalebef31@gmail.com>
2023-06-26 21:28:32 -0300

    passando id cmo parametro

--- src/app/interfaces/instituicao_interface.ts
+++ src/app/interfaces/instituicao_interface.ts
@@ -1,13 +1,13 @@
 export interface instituicao_interface{
-    id: number,
-    nome: string,
-    email: string,
-    telefone: string,
-    cnpj: string,
-    coordenadas: string,
-    imagem: string,
-    usuario_id: number,
-    descricao: string,
+    id: number;
+    nome: string;
+    email: string;
+    telefone: string;
+    cnpj: string;
+    coordenadas: string;
+    imagem: string;
+    usuario_id: number;
+    descricao: string;
     data_criacao: string
 
 }
