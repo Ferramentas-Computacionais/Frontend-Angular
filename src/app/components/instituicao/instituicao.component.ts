ad86b43f2f5d65e9aeeec2c3e3c370808d4692b4
dertufa <tuliosouza2011@gmail.com>
2023-06-26 22:53:01 -0300

    instituições vistas no detalhe
    Falta o mapa e os projetos dessas instituições

--- src/app/components/instituicao/instituicao.component.ts
+++ src/app/components/instituicao/instituicao.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { InstituicaoService } from 'src/app/services/instituicao.service';
+import { Observable, catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-instituicao',
   templateUrl: './instituicao.component.html',
   styleUrls: ['./instituicao.component.scss']
 })
-export class InstituicaoComponent {
-  id!: number;
-  constructor(private route: ActivatedRoute) {
-    this.id = Number(this.route.snapshot.paramMap.get('id'));
-    console.log(this.id)
-  }
-  
+export class InstituicaoComponent{
+  instituicoes: any[] = [];
+  campanhas: any[] = [];
+  constructor(private instituicaoservice: InstituicaoService ){  
+    this.obterinstituicoes();
+    
+}
+obterinstituicoes() {
+  this.instituicaoservice.obter().subscribe(
+    (response: any[]) => {
+      this.instituicoes = response;
+     
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+}
 
+
+obterinstituicaoPorId(userId: number): Observable<any[]> {
+  return this.instituicaoservice.obter_pelo_id(userId);
+}
 }
