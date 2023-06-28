78f21b4fb30babb2017873cabb53ef4c6b3eeb72
kalebeh2o <kalebef31@gmail.com>
2023-06-28 18:12:59 -0300

    correção dos bugs(falta arrumar o visual)

--- src/app/components/instituicao/instituicao.component.ts
+++ src/app/components/instituicao/instituicao.component.ts
@@ -1,33 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InstituicaoService } from 'src/app/services/instituicao.service';
-import { Observable, catchError, map, of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-instituicao',
   templateUrl: './instituicao.component.html',
   styleUrls: ['./instituicao.component.scss']
 })
-export class InstituicaoComponent{
-  instituicoes: any[] = [];
-  campanhas: any[] = [];
-  constructor(private instituicaoservice: InstituicaoService ){  
-    this.obterinstituicoes();
-    
-}
-obterinstituicoes() {
-  this.instituicaoservice.obter().subscribe(
-    (response: any[]) => {
-      this.instituicoes = response;
-     
-    },
-    (error) => {
-      console.error(error);
-    }
-  );
-}
+export class InstituicaoComponent implements OnInit {
+  instituicoes: any;
 
+  constructor(
+    private instituicaoService: InstituicaoService,
+    private route: ActivatedRoute
+  ) { }
 
-obterinstituicaoPorId(userId: number): Observable<any[]> {
-  return this.instituicaoservice.obter_pelo_id(userId);
-}
+  ngOnInit() {
+    const userId = this.route.snapshot.params['id'];
+    console.log(userId);
+    this.obterInstituicaoPorId(userId)
+  }
+
+  obterInstituicaoPorId(userId: number) {
+    this.instituicaoService.obter_pelo_id(userId).subscribe(
+      (response) => {
+        this.instituicoes = response; // atribui o resultado à variável instituicoes
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
