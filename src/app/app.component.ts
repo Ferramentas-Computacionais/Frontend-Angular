4509fb5a2d90edf14dc096109841db432ba6ce06
dertufa <tuliosouza2011@gmail.com>
2023-07-10 17:21:22 -0300

    Adição do Menu Hamburguer
    Agora o menu hamburguer funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,13 @@ export class AppComponent {
   constructor() {
     
   }
+  //se isso aqui for verdadeiro o elemento que ta linkado com essa classe fica visivel 
+  menuVariable:Boolean = false; 
+  menu_icon_variable:Boolean = false;
+  // função que faz os elementos que eu falei em cima ficarem visiveis
+  openMenu(){
+    this.menuVariable =! this.menuVariable;
+    this.menu_icon_variable =! this.menu_icon_variable;
+  }
+
 }
